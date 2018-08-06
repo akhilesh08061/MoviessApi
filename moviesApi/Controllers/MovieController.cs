6f257622312f96a6dcff888033088b86363330b9
akhilesh <akhilesh08061@outlook.com>
2018-08-06 14:01:54 +0530

    initial commit

---
+++ moviesApi/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using moviesApi.Models;
+
+namespace moviesApi.Controllers
+{
+    [Route("api/Movies")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MoviesDataContext _context;
+
+        public MovieController(MoviesDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Movie>> getMovies()    
+        {
+            return _context.Movies.ToList();
+        }
+
+    }
+}
---
+++ moviesApi/Models/MoviesDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace moviesApi.Models
+{
+    public class MoviesDataContext:DbContext
+    {
+        public MoviesDataContext(DbContextOptions<MoviesDataContext> options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
